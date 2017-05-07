a468461ce6eaba4e34eb4c0d515e341c9d494fdc
Nic Jackson <jackson.nic@gmail.com>
2017-05-07 10:53:00 +0100

    Updated elasticbeanstalk to use provider env vars

--- memcache/memcache.tf
+++ memcache/memcache.tf
@@ -27,10 +27,12 @@ resource "aws_elasticache_cluster" "default" {
   engine               = "memcached"
   node_type            = "cache.t2.micro"
   port                 = 11211
-  num_cache_nodes      = "${var.nodes}"
   parameter_group_name = "default.memcached1.4"
+  num_cache_nodes      = "${var.nodes}"
 
   subnet_group_name  = "${aws_elasticache_subnet_group.default.name}"
   security_group_ids = ["${aws_security_group.default.id}"]
   az_mode            = "cross-az"
+
+  notification_topic_arn = "${aws_sns_topic.default.arn}"
 }
